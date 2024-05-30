861503cc7dd46568198fde3a06ddbf1f099dea09
Artur <arturrodriguesmota@hotmail.com>
2024-05-30 19:21:53 -0300

    4: Testing basic functions in c# and unity

--- UnityHelloWorld/Assets/Scripts/HelloWorld.cs
+++ UnityHelloWorld/Assets/Scripts/HelloWorld.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public string publicVariable = "Public Variable";
+    private string privateVariable = "Private Variable";
+    string global = "Global Variable";
     void Start()
     {
         Debug.Log("Hello World - Debug Log");
@@ -14,10 +17,23 @@ public class NewBehaviourScript : MonoBehaviour
         string name = "John Doe";
         float pi = 3.14f;
         Debug.Log("Number: " + number + ", Name: " + name + ", PI: " + pi);
+
+        // Conditional functions
+        if (number > 10)
+        {
+            Debug.Log("Number is greater than 10");
+        }
+        else if (number < 10)
+        {
+            Debug.Log("Number is less than 10");
+        }
+        else
+        {
+            Debug.Log("Number is equal to 10");
+        }
     }
 
     void Update()
     {
-        
     }
 }
