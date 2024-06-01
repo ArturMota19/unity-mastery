b9c83ea1430276055fe9ec63333bd27cb438a7f7
Artur <arturrodriguesmota@hotmail.com>
2024-06-01 11:33:10 -0300

    7: add walls and speed to the ball

---
+++ PongGame/Assets/Scripts/BallController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Rigidbody2D ballRB;
+    public Vector2 ballVelocity;
+    private float speed = 5f;
+
+    
+    void Start()
+    {   
+        ballVelocity.x = -speed;
+        ballRB.velocity = ballVelocity;
+    }
+
+    void Update()
+    {
+    }
+}
