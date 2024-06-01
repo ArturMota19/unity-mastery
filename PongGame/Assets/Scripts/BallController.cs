31ac0a41cbfc0ed193d28704f81ff75005073bc5
Artur <arturrodriguesmota@hotmail.com>
2024-06-01 14:37:42 -0300

    13: Pong, first game w/ unity

--- PongGame/Assets/Scripts/BallController.cs
+++ PongGame/Assets/Scripts/BallController.cs
@@ -8,24 +8,40 @@ public class BallController : MonoBehaviour
     public Rigidbody2D ballRB;
     public Vector2 ballVelocity;
     private float speed = 10f;
+    public AudioClip boing;
+
+    public Transform cameraTransform;
+    public float timeDelay = 1f;
+    public bool isPlaying = false;
 
     
     void Start()
     {   
-        // random to generate 1 or -1
-        float range = Random.Range(0, 2) == 0 ? -1 : 1;
-
-        ballVelocity.x = range * speed; // ball goes to the left or right
-        ballVelocity.y = range * speed; // ball goes up or down
-        ballRB.velocity = ballVelocity;
     }
 
     void Update()
     {
+        // ball w/ delay
+        timeDelay -= Time.deltaTime;
+
+        if(timeDelay <= 0 && !isPlaying){
+            isPlaying = true;
+            // random to generate 1 or -1
+            float range = Random.Range(0, 2) == 0 ? -1 : 1;
+
+            ballVelocity.x = range * speed; // ball goes to the left or right
+            ballVelocity.y = range * speed; // ball goes up or down
+            ballRB.velocity = ballVelocity;
+        }
         if(
             transform.position.x > 8.2 || transform.position.x < -8.2){
             SceneManager.LoadScene("Pong");
         }
         
+    }
+
+    // sound effect
+    private void OnCollisionEnter2D(Collision2D other) {
+        AudioSource.PlayClipAtPoint(boing, cameraTransform.position);
     }
 }
